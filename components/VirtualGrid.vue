32f101b9666501e3d155e24ae605e69ed4213f8e
Warm Beer <mickvd99@gmail.com>
2023-05-08 18:55:31 +0200

    test

--- components/VirtualGrid.vue
+++ components/VirtualGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-2 overflow-x-hidden overflow-y-auto w-full" :class="containerClasses" ref="container" @scroll="handleScroll">
+  <div class="px-2 overflow-x-hidden overflow-y-auto w-full scroll-container" :class="containerClasses" ref="container" @scroll="handleScroll">
     <div class="mt-1 lg:mt-0 w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-1">
       <template v-for="(item, index) in visibleItems">
         <slot :item="item" :index="index"></slot>
@@ -68,6 +68,10 @@ const visibleItems = computed(() => {
 </script>
 
 <style scoped>
+.scroll-container {
+  scroll-padding-top: env(safe-area-inset-top);
+}
+
 .mobile {
   max-height: calc(100vh - 176px);
 }
