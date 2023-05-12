463063fa3aa722505900183fde0f6124aaf31a61
Warm Beer <mickvd99@gmail.com>
2023-05-12 13:29:18 +0200

    filters

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-2 py-2 sticky top-[74px] md:top-0 lg:relative inline-flex justify-center gap-2 bg-neutral-900/90 backdrop-blur-lg lg:bg-transparent z-10 w-full border-b border-neutral-800 shadow-lg lg:shadow-none">
+  <div class="px-2 py-2 sticky top-[73px] md:top-0 lg:relative inline-flex justify-center gap-2 bg-neutral-900/90 backdrop-blur-lg lg:bg-transparent z-10 w-full border-b border-neutral-800 shadow-lg lg:shadow-none">
     <slot></slot>
   </div>
 </template>
