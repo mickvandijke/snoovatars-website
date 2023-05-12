9072a8b56e7ab7bfc3f074dcc26e6e42c0b16472
Warm Beer <mickvd99@gmail.com>
2023-05-12 10:22:23 +0200

    cone

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -211,7 +211,33 @@ function coneInLocalCurrency(eth: number): string {
 
   let symbol = currencyFormatter.formatToParts(0)[0].value;
 
-  return `${symbol}${price.toLocaleString(localeString, { minimumFractionDigits: 10 })}`;
+  let fullString = `${symbol}${price.toLocaleString(localeString, { minimumFractionDigits: 10 })}`;
+
+  let zeroDecimalsString = '';
+
+  let skipLength = symbol.length + 3
+
+  for (let i = skipLength; i < fullString.length; i++) {
+    const char = fullString.charAt(i);
+
+    if (/[1-9]/.test(char)) { // Stop when the character is a number higher than 0
+      break;
+    } else {
+      zeroDecimalsString += char;
+    }
+  }
+
+  const smallCharacters = ["₀", "₁", "₂", "₃", "₄", "₅", "₆", "₇", "₈", "₉", "₁₀"];
+
+  let shortenedString = fullString.slice(symbol.length + 3, fullString.length);
+
+  if (zeroDecimalsString.length > 1 && zeroDecimalsString.length < 10) {
+    shortenedString = shortenedString.replace(zeroDecimalsString, smallCharacters[zeroDecimalsString.length + 1]);
+  }
+
+  shortenedString = `${fullString.slice(0, symbol.length + 3)}${shortenedString}`;
+
+  return shortenedString;
 }
 </script>
 
