fa44a4d8d1345c2bbb2f04c7f079516026f2b035
Warm Beer <mickvd99@gmail.com>
2023-05-18 01:14:33 +0200

    overflow

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-2 py-2 sticky top-[73px] md:top-0 lg:relative inline-flex justify-center gap-2 bg-neutral-900/90 backdrop-blur-lg lg:bg-transparent z-10 w-full border-b border-neutral-800 shadow-lg lg:shadow-none max-w-full">
+  <div class="px-2 py-2 sticky top-[73px] md:top-0 lg:relative inline-flex justify-center gap-2 bg-neutral-900/90 backdrop-blur-lg lg:bg-transparent z-10 w-full border-b border-neutral-800 shadow-lg lg:shadow-none" style="max-width: 100vw;">
     <slot></slot>
   </div>
 </template>
