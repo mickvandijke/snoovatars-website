fa646e7721b911c3341461e79e5926e04d68f617
Warm Beer <mickvd99@gmail.com>
2023-05-29 19:18:48 +0200

    feat: privacy policy

---
+++ pages/privacy.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="px-4 md:px-6 py-12 flex flex-col gap-3 text-neutral-200 max-w-2xl">
+    <h1>Privacy Policy RCAX</h1>
+    <p><strong>Last Updated:</strong> 29-05-2023</p>
+    <p>This privacy policy describes how RCAX ("we," "us," "our") collects, uses, and protects personal information provided by users ("you," "your") of our app RCAX and website RCAX.io. We take your privacy seriously and strive to protect your personal information. Please read this policy carefully to understand how we use your information.</p>
+
+    <h2>1. Collected Information</h2>
+    <p>We collect the following information from users:</p>
+    <ul>
+      <li>Email addresses: These are collected when you register or enable notifications to provide you with important updates and information.</li>
+      <li>Cookies: Our app and website use cookies to analyze app and website usage through Google Analytics. This information is aggregated and anonymous.</li>
+    </ul>
+
+    <h2>2. Use of Collected Information</h2>
+    <p>We use the collected information as follows:</p>
+    <ul>
+      <li>Email addresses: We use your email address to send you important information and updates related to RCAX. You can unsubscribe from these emails at any time using the unsubscribe link provided at the bottom of the emails.</li>
+      <li>Cookies: The information collected through cookies is used to analyze app and website usage, trends, and demographic data of our users. This helps us improve and customize our app and website to better meet the needs of our users.</li>
+    </ul>
+
+    <h2>3. Data Protection and Security</h2>
+    <p>We implement reasonable technical and organizational measures to protect the personal data of our users. Your passwords are hashed with a salt to ensure their security.</p>
+
+    <h2>4. Data Sharing with Third Parties</h2>
+    <p>We do not share your personal data with third parties unless it is necessary to provide the services you requested or as required by law. Cookies from Google Analytics are used to provide us with aggregated analytics of app and website usage, but your personal data is not shared with Google Analytics.</p>
+
+    <h2>5. Cookies and Tracking Technologies</h2>
+    <p>Our app and website use cookies to analyze app and website usage through Google Analytics. You can choose to decline cookies through your browser settings. However, please note that this may impact certain features of the app and website.</p>
+
+    <h2>6. Changes to the Privacy Policy</h2>
+    <p>We reserve the right to modify this privacy policy at any time. Changes become effective once posted on this page. We recommend that you regularly review our privacy policy for updates.</p>
+
+    <h2>7. Contact Information</h2>
+    <p>If you have any questions or comments about this privacy policy or the way we process your personal data, please contact us via email: <a href="mailto:mick@dutchbits.nl">mick@dutchbits.nl</a>.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "privacy"
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  @apply font-bold;
+}
+</style>
