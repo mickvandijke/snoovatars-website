2573d99f2b33cfce0280a027ce2602325fe96527
Warm Beer <mickvd99@gmail.com>
2023-08-20 16:30:42 +0200

    test nyan banner

--- components/AdvertisementBanner.vue
+++ components/AdvertisementBanner.vue
@@ -1,58 +1,58 @@
 <template>
   <div v-if="isBannerShowing" class="relative md:mb-6 px-2 sm:px-6 w-full">
     <Carousel :items-to-show="1" :autoplay="1200000" :wrap-around="true">
-<!--      <Slide :key="1">-->
-<!--        <div class="carousel__item">-->
-<!--          <div class="advertisement">-->
-<!--            <div class="relative p-4 flex gap-12 justify-center items-center w-full h-full overflow-hidden">-->
-<!--              <div class="hidden md:flex justify-center items-center z-20">-->
-<!--                <img src="/images/banners/nyan_banner.png" class="object-contain md:max-h-64 rounded-2xl shadow-xl">-->
-<!--              </div>-->
-<!--              <div class="py-2 md:py-0 relative flex flex-col gap-2 md:gap-6 max-w-md text-left items-center md:items-start z-20">-->
-<!--                <h1 class="w-full md:w-fit text-white text-xl md:text-5xl font-bold z-10">NyanMatcher</h1>-->
-
-<!--                <p class="text-white text-[0.8rem] sm:text-[1rem] font-medium max-w-sm sm:max-w-full z-10">Find the cheapest matching Nyan Cat & Fiesta Dog pairs listed on OpenSea! <span class="inline-flex md:hidden ml-1 w-fit px-1 py-0.5 bg-primary-accent backdrop-blur-lg text-[0.65rem] text-white/60 rounded-lg cursor-pointer">Click to Open</span></p>-->
-
-<!--                <a href="https://nyanmatcher.xyz/" target="_blank" class="hidden md:block p-3 md:p-4 w-fit bg-white hover:bg-white/90 text-black/90 font-semibold rounded-2xl duration-200 z-10">Get Yours</a>-->
-<!--                <div class="absolute left-0 top-0 right-0 bottom-0 bg-[#003466] blur-3xl z-0"></div>-->
-<!--              </div>-->
-<!--              <div alt="Banner Image" class="rainbow-gradient-to-br absolute -left-8 -top-8 -right-8 -bottom-8 blur-lg opacity-100 z-5" />-->
-<!--              <div class="absolute left-0.5 top-0.5 bottom-0.5 right-0.5 sm:left-1 sm:top-1 sm:bottom-1 sm:right-1 overflow-hidden rounded-lg sm:rounded-2xl z-10">-->
-<!--                <img src="/images/banners/nyan_banner_bg.png" alt="Banner Image" class="object-fill object-top" />-->
-<!--              </div>-->
-<!--              <a href="https://nyanmatcher.xyz/" target="_blank" class="md:hidden absolute top-0 left-0 right-0 bottom-0 z-20"></a>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </Slide>-->
       <Slide :key="1">
         <div class="carousel__item">
-          <div class="advertisement bg-black">
-            <div class="flex gap-12 items-center">
-              <div class="hidden md:flex justify-center items-center">
-                <img src="/images/banners/rcax_icon_banner.png" class="object-contain md:max-h-64 rounded-2xl shadow-xl">
+          <div class="advertisement">
+            <div class="relative p-4 flex gap-12 justify-center items-center w-full h-full overflow-hidden">
+              <div class="hidden md:flex justify-center items-center z-20">
+                <img src="/images/banners/nyan_banner.png" class="object-contain md:max-h-64 rounded-2xl shadow-xl">
               </div>
-              <div class="flex md:flex-col gap-6 max-w-md text-left items-center md:items-start">
-                <h1 class="text-white text-xl md:text-5xl font-bold">Your Banner Here?</h1>
+              <div class="py-2 md:py-0 relative flex flex-col gap-2 md:gap-6 max-w-md text-left items-center md:items-start z-20">
+                <h1 class="w-full md:w-fit text-white text-xl md:text-5xl font-bold z-10">NyanMatcher</h1>
 
-                <p class="hidden md:block text-white font-medium">Sponsor RCAX's development by renting a banner. Prices start from $5 a day.</p>
+                <p class="text-white text-[0.8rem] sm:text-[1rem] font-medium max-w-sm sm:max-w-full z-10">Find the cheapest matching Nyan Cat & Fiesta Dog pairs listed on OpenSea! <span class="inline-flex md:hidden ml-1 w-fit px-1 py-0.5 bg-primary-accent backdrop-blur-lg text-[0.65rem] text-white/60 rounded-lg cursor-pointer">Click to Open</span></p>
 
-                <NuxtLink to="/sponsoring" class="p-3 md:p-4 w-fit bg-white hover:bg-white/90 text-black/90 text-sm md:text-md font-semibold rounded-2xl duration-200">Read More</NuxtLink>
+                <a href="https://nyanmatcher.xyz/" target="_blank" class="hidden md:block p-3 md:p-4 w-fit bg-white hover:bg-white/90 text-black/90 font-semibold rounded-2xl duration-200 z-10">Get Yours</a>
+                <div class="absolute left-0 top-0 right-0 bottom-0 bg-[#003466] blur-3xl z-0"></div>
               </div>
+              <div alt="Banner Image" class="rainbow-gradient-to-br absolute -left-8 -top-8 -right-8 -bottom-8 blur-lg opacity-100 z-5" />
+              <div class="absolute left-0.5 top-0.5 bottom-0.5 right-0.5 sm:left-1 sm:top-1 sm:bottom-1 sm:right-1 overflow-hidden rounded-lg sm:rounded-2xl z-10">
+                <img src="/images/banners/nyan_banner_bg.png" alt="Banner Image" class="object-fill object-top" />
+              </div>
+              <a href="https://nyanmatcher.xyz/" target="_blank" class="md:hidden absolute top-0 left-0 right-0 bottom-0 z-20"></a>
             </div>
           </div>
         </div>
       </Slide>
-      <Slide :key="2">
-        <div class="carousel__item">
-          <div class="advertisement relative">
-            <a href="https://www.the23.club" target="_blank" class="flex h-full items-center z-20">
-              <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="object-contain h-full max-h-44" />
-            </a>
-            <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="absolute left-0 top-0 right-0 bottom-0 h-full w-full blur-3xl opacity-50 z-10" />
-          </div>
-        </div>
-      </Slide>
+<!--      <Slide :key="1">-->
+<!--        <div class="carousel__item">-->
+<!--          <div class="advertisement bg-black">-->
+<!--            <div class="flex gap-12 items-center">-->
+<!--              <div class="hidden md:flex justify-center items-center">-->
+<!--                <img src="/images/banners/rcax_icon_banner.png" class="object-contain md:max-h-64 rounded-2xl shadow-xl">-->
+<!--              </div>-->
+<!--              <div class="flex md:flex-col gap-6 max-w-md text-left items-center md:items-start">-->
+<!--                <h1 class="text-white text-xl md:text-5xl font-bold">Your Banner Here?</h1>-->
+
+<!--                <p class="hidden md:block text-white font-medium">Sponsor RCAX's development by renting a banner. Prices start from $5 a day.</p>-->
+
+<!--                <NuxtLink to="/sponsoring" class="p-3 md:p-4 w-fit bg-white hover:bg-white/90 text-black/90 text-sm md:text-md font-semibold rounded-2xl duration-200">Read More</NuxtLink>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </Slide>-->
+<!--      <Slide :key="2">-->
+<!--        <div class="carousel__item">-->
+<!--          <div class="advertisement relative">-->
+<!--            <a href="https://www.the23.club" target="_blank" class="flex h-full items-center z-20">-->
+<!--              <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="object-contain h-full max-h-44" />-->
+<!--            </a>-->
+<!--            <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="absolute left-0 top-0 right-0 bottom-0 h-full w-full blur-3xl opacity-50 z-10" />-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </Slide>-->
 
       <template #addons>
         <Pagination />
