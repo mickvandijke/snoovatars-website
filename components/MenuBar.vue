2b0e55650fd976e3373031e50c38b7d4c45c05bb
Warm Beer <mickvd99@gmail.com>
2023-08-20 19:18:54 +0200

    fix: sponsoring page

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="menubar px-2 py-2 sm:px-6 sm:py-4 inline-flex justify-center sm:justify-start gap-2 sm:gap-4 z-10 w-full" style="max-width: 100vw;">
+  <div class="menubar px-2 py-2 sm:px-6 sm:py-4 inline-flex justify-center sm:justify-start gap-2 sm:gap-4 z-10 w-full overflow-hidden" style="max-width: 100%;">
     <slot></slot>
   </div>
 </template>
@@ -14,6 +14,6 @@
 }
 
 .menubar > select {
-  @apply p-2 bg-primary-accent hover:bg-primary-accent-hover text-sm text-white placeholder-white/20 font-medium border-none focus:outline-none sm:w-fit max-w-sm overflow-x-hidden rounded-lg;
+  @apply p-2 bg-primary-accent hover:bg-primary-accent-hover text-sm text-white placeholder-white/20 font-medium border-none focus:outline-none w-full max-w-[6rem] md:max-w-fit rounded-lg;
 }
 </style>
--- pages/sponsoring.vue
+++ pages/sponsoring.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-4 md:px-6 py-12 flex flex-col gap-3 text-neutral-200 w-full max-w-2xl">
+  <div class="px-4 md:px-6 py-12 flex flex-col gap-3 text-neutral-200 max-w-2xl">
     <h1 class="text-2xl">Sponsor RCAX's Development</h1>
 
     <p>You can sponsor RCAX by renting a advertisement banner. Prices start from $5 a day.</p>
@@ -18,36 +18,16 @@
 
     <p>If you want to sponsor RCAX's development anonymously, you can do so by sending something to:</p>
 
-    <p>ETH: <span class="font-bold">0xB0753614730408d91Ccc46c1f4210efF0bee648B</span></p>
+    <p>ETH: <span class="font-bold break-all">0xB0753614730408d91Ccc46c1f4210efF0bee648B</span></p>
 
-    <p>BTC: <span class="font-bold">bc1qpztwrlapl330gn74uqn3p03egekdpxcvkv7vtk</span></p>
+    <p>BTC: <span class="font-bold break-all">bc1qpztwrlapl330gn74uqn3p03egekdpxcvkv7vtk</span></p>
 
     <p class="italic">Thank you !!! :D</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from "#imports";
-import {deleteUser} from "~/composables/api/user";
 
-const usernameOrEmail = ref("");
-const loading = ref(false);
-
-function submitRequest() {
-  if (loading.value) {
-    return;
-  }
-
-  deleteUser(usernameOrEmail.value)
-      .then(async (message) => {
-        loading.value = false;
-        alert(message);
-      })
-      .catch((err) => {
-        loading.value = false;
-        alert(err);
-      });
-}
 </script>
 
 <style scoped>
