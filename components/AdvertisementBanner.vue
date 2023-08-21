f02a713908c2a0321ec099baa2fe5e1ab2c5cb98
Warm Beer <mickvd99@gmail.com>
2023-08-21 11:05:44 +0200

    chore: add hodl banner back

--- components/AdvertisementBanner.vue
+++ components/AdvertisementBanner.vue
@@ -49,16 +49,16 @@
           </div>
         </div>
       </Slide>
-<!--      <Slide :key="3">-->
-<!--        <div class="carousel__item">-->
-<!--          <div class="advertisement relative">-->
-<!--            <a href="https://www.the23.club" target="_blank" class="flex h-full items-center z-20">-->
-<!--              <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="object-contain h-full max-h-44" />-->
-<!--            </a>-->
-<!--            <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="absolute left-0 top-0 right-0 bottom-0 h-full w-full blur-3xl opacity-50 z-10" />-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </Slide>-->
+      <Slide :key="3">
+        <div class="carousel__item">
+          <div class="advertisement relative">
+            <a href="https://www.the23.club" target="_blank" class="flex h-full items-center z-20">
+              <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="object-contain h-full max-h-44" />
+            </a>
+            <img src="/images/banners/hodl_banner2.png" alt="Banner Image" class="absolute left-0 top-0 right-0 bottom-0 h-full w-full blur-3xl opacity-50 z-10" />
+          </div>
+        </div>
+      </Slide>
 
       <template #addons>
         <Pagination />
