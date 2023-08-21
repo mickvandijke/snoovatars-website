f5ad92863aba9116199fe88db003815934be989f
Warm Beer <mickvd99@gmail.com>
2023-08-21 03:25:48 +0200

    chore: renamed l5s to 5 sales avg

--- components/SeriesStatsComponentItem.vue
+++ components/SeriesStatsComponentItem.vue
@@ -165,7 +165,7 @@
                 </div>
               </template>
               <template v-else>
-                <span class="text-white/60 font-medium">Last 5 Sales:</span>
+                <span class="text-white/60 font-medium">5 Sales Avg:</span>
                 <div class="flex gap-0.25 items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" fill="currentColor" class="w-3 h-3 text-white/60"><path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
                   <div class="text-neutral-200">{{ item.stats.five_last_sales_average.toFixed(4).replace(/\.?0+$/, '') }}</div>
