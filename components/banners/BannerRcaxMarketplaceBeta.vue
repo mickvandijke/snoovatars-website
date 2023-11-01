54152949d780e80e417bce8269c733cfe09bd416
Warm Beer <mickvd99@gmail.com>
2023-11-01 16:49:11 +0100

    text

--- components/banners/BannerRcaxMarketplaceBeta.vue
+++ components/banners/BannerRcaxMarketplaceBeta.vue
@@ -8,7 +8,7 @@
         <div class="py-2 md:py-0 relative flex flex-col gap-2 md:gap-6 max-w-md md:max-w-xl text-left items-center md:items-start z-20">
           <h1 class="w-full md:w-fit text-white text-xl md:text-5xl font-bold z-10">RCAX Marketplace BETA</h1>
 
-          <p class="text-white text-[0.8rem] md:text-[1rem] font-medium max-w-sm sm:max-w-full z-10">Introducing the RCAX Marketplace. A marketplace completely centered around Reddit Collectible Avatars with first class <span class="text-amber-500">$RCAX</span> support. <span class="inline-flex md:hidden ml-2 w-fit px-1 py-0.5 bg-primary-accent backdrop-blur-lg text-[0.65rem] text-white/60 rounded-lg cursor-pointer">Trade</span></p>
+          <p class="text-white text-[0.8rem] md:text-[1rem] font-medium max-w-sm sm:max-w-full z-10">A marketplace completely centered around Reddit Collectible Avatars with first class <span class="text-amber-500">$RCAX</span> support. <span class="inline-flex md:hidden ml-2 w-fit px-1 py-0.5 bg-primary-accent backdrop-blur-lg text-[0.65rem] text-white/60 rounded-lg cursor-pointer">Trade</span></p>
 
           <a href="https://marketplace.rcax.io/" target="_blank" class="hidden md:block p-3 md:p-4 w-fit bg-white hover:bg-white/90 text-black/90 font-semibold rounded-2xl duration-200 z-10">Trade</a>
           <div class="absolute left-0 top-0 right-0 bottom-0 bg-[#F59E0B]/60 opacity-80 blur-3xl z-0"></div>
