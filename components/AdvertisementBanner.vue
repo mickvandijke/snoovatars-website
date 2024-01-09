7a3afb6ca9e889cd173c04bfd61aa8c5d7388ea0
Warm Beer <mickvd99@gmail.com>
2024-01-09 23:20:21 +0100

    chore: made mobile banner bigger

--- components/AdvertisementBanner.vue
+++ components/AdvertisementBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isBannerShowing" class="relative py-2 md:mb-6 px-2 sm:px-6 w-full">
+  <div v-if="isBannerShowing" class="relative md:mb-6 px-2 sm:px-6 w-full">
     <Carousel :items-to-show="1" :autoplay="10000" :wrap-around="true" pause-autoplay-on-hover>
       <Slide v-for="(slide, i) in shuffledSlides" :key="i">
         <component :is="slide.component" />
