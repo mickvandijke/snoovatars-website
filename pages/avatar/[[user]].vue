9039fd3ebdb138dc609b1674492d8b98d7efc7d6
Warm Beer <mickvd99@gmail.com>
2024-01-20 14:56:03 +0100

    style

--- pages/avatar/[[user]].vue
+++ pages/avatar/[[user]].vue
@@ -58,7 +58,7 @@
         <div class="mt-2 flex flex-col items-center gap-2 text-white/80 w-full max-w-xs">
           <template v-if="!Capacitor.isNativePlatform() && bgSeriesStats">
             <a :href="marketplaceLink(bgSeriesStats)" target="_blank" class="mb-2 py-2 px-3 max-w-full flex items-center gap-1.5 border border-white/10 hover:border-amber-500 rounded-lg duration-300">
-              Buy <span class="text-header font-semibold truncate">{{ selectedBackground.name }}</span> on <img src="/images/branding/rcax/rcaxIcon.svg" class="w-5 h-5">
+              Buy <span class="text-header font-semibold truncate">{{ selectedBackground.name }}</span> on <img src="/images/branding/rcax/RCAX_Badge_Color.svg" class="w-5 h-5">
               <span class="text-sm text-details">({{ ethereumInLocalCurrency(getLowestListingAsGweiPrice(bgSeriesStats)) }})</span>
             </a>
           </template>
